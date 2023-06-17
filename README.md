49e7707d212f78ec2a3f47deeece78d08ab99f9d
Catarina F. M. Castro <catfmcastro@gmail.com>
2023-06-17 18:21:06 -0300

    mexendo nos bagui aqui

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TabelaLogaritmo
-Trabalho elaborado para a disciplina de Cálculo 1. Tem como objetivo a criação de uma tabela interativa para o cálculo de logaritmos sem a utilização de bibliotecas como a "math.h" da linguagem c.
+Trabalho elaborado para a disciplina de Cálculo 1. Tem como objetivo a criação de uma tabela interativa para o cálculo de logaritmos sem a utilização de bibliotecas como a "math.h" da linguagem C.
+
+#### Integrantes
+*Arthur Santos
+*Catarina F. M. Castro
+*Davi Oliveira Sad
+*Matheus Fagundes
--- java.js
+++ java.js
@@ -3,36 +3,19 @@ function Oba () {
 }
 
 
-// Inicio do cálculo do Fagundes para o Ln
 
-function CalcularLn() {
 
-    var valor = parseFloat(document.getElementById("inserir").value);
+function calcularLn() {
+    var valor = parseFloat(document.getElementById("entrada").value);
     var resultado = ln(valor);
-    
-    let str = `
-    <div id="resposta">
-    
-    <h2>Valor de Ln (<span>${valor}</span>)</h2>
-    
-    <h4><p>Valor: ${resultado}</p></h4>
-    
-    </div>`;
-    
-    
-    document.getElementById('resposta').innerHTML = str;
-
+    console.log(parseFloat(resultado));
 }
 
-function ln(x) 
-{
-
-    var termos = 50;
+function ln(x) {
+    var termos = 100;
     var soma = 0;
 
-    for (var n = 1; n <= termos; n++) 
-    {
-
+    for (var n = 1; n <= termos; n++) {
         var numerador = Math.pow(x - 1, n);
         var denominador = n * Math.pow(x, n);
         var termo = numerador / denominador;
@@ -43,7 +26,16 @@ function ln(x)
     return soma;
 }
 
-// Final do Cálculo do Fafa
+function calcularLogX() {
+    var log = parseFloat(document.getElementById("valor").value);
+    var base = parseFloat(document.getElementById("base").value);
+    var resultadoLog = logX(log, base);
+    console.log(parseFloat(resultadoLog));
+}
 
+function logX(log, base) {
+    var numero = ln(log) / ln(base);
+    return numero;
+}
 
 
